46199e85b1cb85e8413340d60d91bc6987db83ab
kiranmai1111 <98233835+kiranmai1111@users.noreply.github.com>
2024-10-03 22:31:53 +0530

    newchatbot.py

--- newchatbot.py
+++ newchatbot.py
@@ -11,11 +11,11 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from io import BytesIO
 import base64
-import boto3
 
 # Load the API Key
 load_dotenv()
 genai.configure(api_key=os.getenv("AIzaSyCtyGp4yXkmsy06LmyXDUh6dpcnxO00bsc"))
+
 
 # Function to read PDF file
 def read_pdf(pdf):
@@ -26,11 +26,13 @@ def read_pdf(pdf):
             text += page.extract_text()
     return text
 
+
 # Document Chunking
 def get_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
+
 
 # Create Embedding Store
 def get_vector_store(text_chunks):
@@ -38,6 +40,7 @@ def get_vector_store(text_chunks):
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
     return vector_store
+
 
 # Create Conversation Chain
 def get_conversation_chain_pdf():
@@ -54,13 +57,6 @@ def get_conversation_chain_pdf():
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
-# Function to download PDF from S3
-def download_pdf_from_s3(bucket_name, file_key):
-    s3 = boto3.client('s3')
-    pdf_file = BytesIO()
-    s3.download_fileobj(bucket_name, file_key, pdf_file)
-    pdf_file.seek(0)  # Move to the beginning of the BytesIO object
-    return pdf_file
 
 # Processing User Input
 def user_input(user_query):
@@ -71,24 +67,34 @@ def user_input(user_query):
     response = chain.run(input_documents=docs, question=user_query)
     st.write(response)
 
+
+
+
+
 def main():
     # Set up the main UI
     st.header("Welcome to Mind and Muscle, Ask Anything")
 
-    # S3 bucket and file details
-    bucket_name = 'botbot12345'  # Your S3 bucket name
-    file_key = 'Welcome to Mind and Muscle.pdf'  # Your file key in S3
+    # Set the background image
 
-    # Download the PDF file from S3
-    pdf_file = download_pdf_from_s3(bucket_name, file_key)
+    # Example: Load a PDF file programmatically
+    pdf_file_path ='./Welcome to Mind and Muscle.pdf'
 
-    raw_text = read_pdf([pdf_file])  # Read the PDF file
-    text_chunks = get_chunks(raw_text)  # Chunk the text
-    get_vector_store(text_chunks)  # Store embeddings
+
+
+
+    with open(pdf_file_path, 'rb') as f:
+        pdf_file = BytesIO(f.read())
+        pdf_file.name = os.path.basename(pdf_file_path)  # Set a name for the file
+
+        raw_text = read_pdf([pdf_file])  # Read the PDF file
+        text_chunks = get_chunks(raw_text)  # Chunk the text
+        get_vector_store(text_chunks)  # Store embeddings
 
     user_query = st.text_input("Drop your Question")
     if user_query:
         user_input(user_query)
+
 
 if __name__ == "__main__":
     main()
